fee8fd42fd34a120bf295ce24207b21ca073393e
jaeho0613 <ywm2004@gmail.com>
2024-10-22 22:32:27 +0900

    섹션5 - 프록시 패턴과 데코레이터 패턴
    프록시 패턴 - 예제 코드2

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/code/RealSubject.java
@@ -0,0 +1,15 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static hello.proxy.Util.sleep;
+
+@Slf4j
+public class RealSubject implements Subject {
+    @Override
+    public String operation() {
+        log.info("실제 객체 호출");
+        sleep(1000);
+        return "data";
+    }
+}
