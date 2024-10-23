0c195d22cb2a2062080b06419a74d81e82a6e54c
jaeho0613 <ywm2004@gmail.com>
2024-10-23 22:44:47 +0900

    섹션5 - 프록시 패턴과 데코레이터 패턴
    구체 클래스 기반 프록시 - 예제2

--- proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreateproxy;
 
 import hello.proxy.pureproxy.concreateproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreateproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreateproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogic logic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(logic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        // ConcreteLogic logic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy();
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
